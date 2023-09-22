911004debae766a75c274be4882d0eda135461fa
MonoDG <monodg@proton.me>
2023-09-22 14:14:59 -0400

    Add operate function

--- js/script.js
+++ js/script.js
@@ -17,3 +17,18 @@ function divide(a, b) {
 
     return a / b;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    let result = null;
+    switch (operator) {
+        case "+": result = add(firstNumber, secondNumber); break;
+        case "-": result = substract(firstNumber, secondNumber); break;
+        case "*": result = multiply(firstNumber, secondNumber); break;
+        case "/": result = divide(firstNumber, secondNumber); break;
+    }
+    return result;
+}
